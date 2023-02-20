bb9ddbc2e0cc7f58f764775225f25f02ea35cce5
George Joseph <71743801+GeorgeJoseph5000@users.noreply.github.com>
2023-02-20 15:25:26 +0200

    Update statefulWidgetExample.dart

--- statefulWidgetExample.dart
+++ statefulWidgetExample.dart
@@ -5,6 +5,36 @@ void main() {
   runApp(const MyApp());
 }
 
+class Styles {
+  static ThemeData themeData(bool isDarkTheme, BuildContext context) {
+    return ThemeData(
+      primarySwatch: Colors.blue,
+      primaryColor: !isDarkTheme ? Colors.black : Colors.white,
+      backgroundColor: isDarkTheme ? Colors.black : Color(0xffF1F5FB),
+      indicatorColor: isDarkTheme ? Color(0xff0E1D36) : Color(0xffCBDCF8),
+      buttonColor: isDarkTheme ? Color(0xff3B3B3B) : Color(0xffF1F5FB),
+      hintColor: isDarkTheme ? Colors.blue : Color(0xffEECED3),
+      highlightColor: isDarkTheme
+          ? Color.fromARGB(255, 1, 32, 55)
+          : Color.fromARGB(255, 146, 206, 252),
+      hoverColor: isDarkTheme ? Color(0xff3A3A3B) : Color(0xff4285F4),
+      focusColor: isDarkTheme ? Color(0xff0B2512) : Color(0xffA8DAB5),
+      disabledColor: Colors.grey,
+      textSelectionTheme: TextSelectionThemeData(
+          selectionColor: isDarkTheme ? Colors.white : Colors.blueAccent),
+      cardColor: isDarkTheme ? Color(0xFF151515) : Colors.white,
+      canvasColor: isDarkTheme ? Colors.black : Colors.grey[50],
+      brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+      buttonTheme: Theme.of(context).buttonTheme.copyWith(
+          colorScheme: isDarkTheme ? ColorScheme.dark() : ColorScheme.light()),
+      appBarTheme: AppBarTheme(
+        elevation: 0.0,
+      ),
+    );
+  }
+}
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,9 +43,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: Styles.themeData(true, context),
       home: MyHomePage(),
     );
   }
@@ -24,7 +52,15 @@ class MyApp extends StatelessWidget {
 class ProfileCard extends StatelessWidget {
   final String imageUrl;
   final String name;
-  const ProfileCard({super.key, required this.imageUrl, required this.name});
+  final TimeOfDay timeOfDay;
+  final DateTime dateTime;
+  
+  const ProfileCard(
+      {super.key,
+      required this.imageUrl,
+      required this.name,
+      required this.timeOfDay,
+      required this.dateTime});
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +70,6 @@ class ProfileCard extends StatelessWidget {
           Container(
             margin: EdgeInsets.all(7),
             child: Stack(
-              
               alignment: const Alignment(0.6, 0.6),
               children: [
                 CircleAvatar(
@@ -63,22 +98,30 @@ class ProfileCard extends StatelessWidget {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-          Text(name, style: TextStyle(fontSize: 20),),
-          RatingBar.builder(
-            initialRating: 3,
-            minRating: 1,
-            direction: Axis.horizontal,
-            allowHalfRating: true,
-            itemCount: 4,
-            itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-            itemBuilder: (context, _) => Icon(
-              Icons.star,
-              color: Colors.amber,
-            ),
-            onRatingUpdate: (rating) {
-              print(rating);
-            },
-          )],)
+              Text(
+                name,
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 20),
+              Text(timeOfDay.format(context)),
+              Text(dateTime.day.toString() + "/" + dateTime.month.toString() + "/" + dateTime.year.toString()),
+              RatingBar.builder(
+                initialRating: 3,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 4,
+                itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                itemBuilder: (context, _) => Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                ),
+                onRatingUpdate: (rating) {
+                  print(rating);
+                },
+              )
+            ],
+          )
         ],
       ),
     );
@@ -86,6 +129,12 @@ class ProfileCard extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  String name = "Beast";
+  String imageUrl =
+      "https://pbs.twimg.com/profile_images/994592419705274369/RLplF55e_400x400.jpg";
+  TimeOfDay timeInDay = TimeOfDay(hour: 0, minute: 0);
+  DateTime date = DateTime.now();
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -133,10 +182,82 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       appBar: AppBar(title: const Text("Flutter App")),
       body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        TextFormField(
+          decoration: InputDecoration(
+              label: Text("Name"), prefixIcon: Icon(Icons.person)),
+          onChanged: (value) {
+            setState(() {
+              widget.name = value;
+            });
+          },
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+              label: Text("Image Url"), prefixIcon: Icon(Icons.cloud)),
+          onChanged: (value) {
+            setState(() {
+              widget.imageUrl = value;
+            });
+          },
+        ),
+        SizedBox(
+          height: 50,
+        ),
         ProfileCard(
-            imageUrl:
-                "https://pbs.twimg.com/profile_images/994592419705274369/RLplF55e_400x400.jpg",
-            name: "Beast")
+            imageUrl: widget.imageUrl,
+            name: widget.name,
+            timeOfDay: widget.timeInDay,
+            dateTime: widget.date),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      TimeOfDay? newTime = await showTimePicker(
+                        context: context,
+                        initialTime: const TimeOfDay(hour: 0, minute: 0),
+                      );
+                      setState(() {
+                        if (newTime != null) {
+                          widget.timeInDay = newTime;
+                        }
+                      });
+                      print(newTime);
+                    },
+                    child: Text("Choose Time")),
+                Center(
+                    child: Text(widget.timeInDay.toString() == "null"
+                        ? ""
+                        : widget.timeInDay.format(context))),
+              ],
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      DateTime? newDate = await showDatePicker(
+                        initialDate: DateTime.now(),
+                        context: context, firstDate: DateTime(1990), lastDate: DateTime(2029),
+                      );
+                      setState(() {
+                        if (newDate != null) {
+                          widget.date = newDate;
+                        }
+                      });
+                      print(newDate);
+                    },
+                    child: Text("Choose Date")),
+                Center(
+                    child: Text(widget.date.toString() == "null"
+                        ? ""
+                        : widget.date.day.toString() + "/" + widget.date.month.toString() + "/" + widget.date.year.toString())),
+              ],
+            ),
+            
+          ],
+        ),
       ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
